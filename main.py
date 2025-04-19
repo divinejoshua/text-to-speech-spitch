1df8523eba411820cf11db89d1fb2ef5fde30b07
divinejoshua <divineuche399@gmail.com>
2025-04-19 14:01:54 +0100

    Added text to speech

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 from spitch import Spitch
 
-os.environ["SPITCH_API_KEY"] = "YOUR_API_KEY"
+
+os.environ["SPITCH_API_KEY"] = os.getenv("SPITCH_API_KEY")
 client = Spitch()
 
 with open("new.mp3", "wb") as f:
     response = client.speech.generate(
-        text="Bawo ni ololufe mi?",
-        language="yo",
-        voice="sade"
+        text="Movies, oh my gosh, I just just absolutely love them. They are like time machines taking you to different worlds and landscapes, and um, and I just cant get enough of it.",
+        language="en",
+        voice="lucy",
     )
     f.write(response.read())
